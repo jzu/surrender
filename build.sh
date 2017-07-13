ac8cbe5ad78681b1eb6c63092065dae4ed975079
Jean Zundel <jzu@free.fr>
2017-07-13 17:38:51 +0200

    Script

--- build.sh
+++ build.sh
@@ -5,22 +5,37 @@ export LC_ALL=C
 
 . ./includes.sh
 
+
+# Grab the source 
+
 git pull | grep up-to-date 
 if [ $? -eq 0 ]
 then
   exit 0
 fi
 
-pdflatex -halt-on-error $SRC_FILE
+
+# Shia Labeouf
+
+$COMMAND
 if [ $? -ne 0 ]
 then
   exit 1
 fi
 
-/bin/mv -f $SRC_FILE /tmp/source.$$
-/bin/mv -f $DST_FILE /tmp/dest.$$
-/bin/rm -f $PROJECT.*
-/bin/mv -f /tmp/source.$$ $SRC_FILE
-/bin/mv -f /tmp/dest.$$ $DST_FILE
+
+# Send back the result
+
+git add "$DST_FILE"
+git commit -m "Automatically built by https://github.com/jzu/surrender"
+git push
+
+# Cleanup
+
+/bin/mv -f "$SRC_FILE" /tmp/source.$$
+/bin/mv -f "$DST_FILE" /tmp/dest.$$
+/bin/rm -f "$PROJECT".*
+/bin/mv -f /tmp/source.$$ "$SRC_FILE"
+/bin/mv -f /tmp/dest.$$ "$DST_FILE"
 
 
